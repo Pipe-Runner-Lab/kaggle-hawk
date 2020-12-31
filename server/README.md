f72fc20993978d5a83c560c6ac55c7ee7f57c80a
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2020-12-31 16:45:13 +0530

    [AakashMallik] Kaggle parser adv code added | not working

--- server/README.md
+++ server/README.md
@@ -1,3 +1,4 @@
 ## Resources
 
 - https://www.freecodecamp.org/news/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3/
+- https://flaviocopes.com/web-scraping/
--- server/src/parser/kaggle.ts
+++ server/src/parser/kaggle.ts
@@ -3,11 +3,36 @@ import { KAGGLE_CONTEST_PAGE } from './urls';
 
 export const generateKaggleJSON = async (): Promise<void> => {
   const browser = await puppeteer.launch();
-  const newPage = await browser.newPage();
-  await newPage.goto(KAGGLE_CONTEST_PAGE);
-  const HTMLContent = await newPage.content;
-  console.log('HTML Content follows');
-  console.log(HTMLContent);
+  const page = await browser.newPage();
+  await page.goto(KAGGLE_CONTEST_PAGE, {
+    waitUntil: ['networkidle2'],
+  });
+  const competitionList = await page.evaluate(() => {
+    const listItems = document.querySelectorAll(
+      'div.sc-pkryX.tCtqx li.mdc-list-item',
+    );
+    const data = [];
+    listItems.forEach((item) => {
+      const title = item.querySelector('.mdc-list-item__primary-text')
+        .textContent;
+      const description = item.querySelector('.hMdDCJ').textContent;
+      const link = item.querySelector('a').href;
+      const iconUrl = item.querySelector('img').src;
+      const secondaryDescription = item.querySelector('.jTyItw').textContent;
 
+      console.log(title);
+      data.concat({
+        title,
+        description,
+        link,
+        iconUrl,
+        secondaryDescription,
+      });
+    });
+
+    return data;
+  });
+
+  console.log(competitionList);
   await browser.close();
 };
