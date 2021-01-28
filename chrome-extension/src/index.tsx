62f5058d85c66169688db716187a3e129119b911
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-01-28 19:47:11 +0530

    Setup react, typescript and babel

---
+++ chrome-extension/src/index.tsx
@@ -0,0 +1,6 @@
+import _ from "lodash";
+import React from 'react';
+import ReactDOM from "react-dom";
+import App from "./app";
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
---
+++ chrome-extension/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  // plugins: [
+  //   new CleanWebpackPlugin(),
+  //   new HtmlWebpackPlugin({
+  //     title: "Kaggle Hawk",
+  //     template: path.resolve(__dirname, "public","index.html"),
+  //   }),
+  // ],
+};
