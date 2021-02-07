cac10af3f88f5b7065f9cbc1d9b5d443b6a919e1
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-02-07 16:49:34 +0530

    Added concurrently to dev-dependencies

--- chrome-extension/webpack.config.fg.js
+++ chrome-extension/webpack.config.fg.js
@@ -13,7 +13,14 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.fg.json",
+            },
+          },
+        ],
         exclude: /node_modules/,
       },
       {
